1aadbcb5cbdf227667bd52f2cb5d78f2d34db446
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2021-12-20 12:08:53 -0600

    created basic styles for social icons

--- src/components/Social-Icons.js
+++ src/components/Social-Icons.js
@@ -4,11 +4,11 @@ import { faTwitch, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faCoffee, faPaintBrush } from "@fortawesome/free-solid-svg-icons";
 function SocialIcons() {
   return (
-    <div>
-      <FontAwesomeIcon icon={faTwitch} />
-      <FontAwesomeIcon icon={faTwitter} />
-      <FontAwesomeIcon icon={faCoffee} />
-      <FontAwesomeIcon icon={faPaintBrush } />
+    <div className="social-icons flex justify-evenly gap-4 my-10">
+      <FontAwesomeIcon icon={faTwitch} size="3x" />
+      <FontAwesomeIcon icon={faTwitter} size="3x" />
+      <FontAwesomeIcon icon={faPaintBrush } size="3x" />
+      <FontAwesomeIcon icon={faCoffee} size="3x" />
     </div>
   );
 }
