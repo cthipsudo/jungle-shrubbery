bf14bc9215d87eb3a0d88e93322af84c298f6bb0
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2021-12-20 14:44:11 -0600

    Refactored Hover effect on pfp

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import pfp from "../images/shiroi-white-trim.png"
+import pfp from "../images/shiroi-white-trim.png";
 
 const Profile = () => {
   function hoverImage(e) {
@@ -8,10 +8,19 @@ const Profile = () => {
   }
   return (
     <div id="profile" className="flex flex-col justify-center items-center ">
-      <img className=" animate__animated animate__fadeIn" onMouseOver={hoverImage} src={pfp}/>
-      <h1 className="text-white text-xl font-bold animate__animated animate__fadeIn"> @jungie_boi </h1>
+      <div className="hov-wrapper flex flex-col items-center">
+        <img
+          className=" animate__animated animate__fadeIn"
+          onMouseOver={hoverImage}
+          src={pfp}
+        />
+        <h1 className="text-white text-xl font-bold animate__animated animate__fadeIn">
+          {" "}
+          @jungie_boi{" "}
+        </h1>
+      </div>
     </div>
   );
-}
+};
 
 export default Profile;
