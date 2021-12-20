b54c7a2815e14e71373f64cb7875eb6086b3e02c
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2021-12-20 14:39:36 -0600

    Added Symbol to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
 import react from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGrimace } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
-  return(
+  return (
     <footer className="c-footer text-center text-white p-5">
-      <p>Developed By <a className="underline decoration-solid" href="https://github.com/cthipsudo">cthipsudo</a></p>
+      <p>
+        Developed By{" "}
+        <a
+          className="underline decoration-solid"
+          href="https://github.com/cthipsudo"
+        >
+          cthipsudo
+        </a>{" "}
+        <FontAwesomeIcon icon={faGrimace} />
+      </p>
     </footer>
   );
 }
