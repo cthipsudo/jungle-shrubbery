38bd4a612d8bb836a7d971079a5549fba679f090
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2021-12-20 12:20:37 -0600

    imported animation libary, animation testing and link hover styling

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,9 +3,9 @@ import pfp from "../images/shiroi-white-trim.png"
 
 const Profile = () => {
   return (
-    <div id="profile" className="flex flex-col justify-center items-center">
-      <img src={pfp}/>
-      <h1 className="text-white text-xl font-bold"> @jungie_boi </h1>
+    <div id="profile" className="flex flex-col justify-center items-center ">
+      <img className="animate__bounceIn" src={pfp}/>
+      <h1 className="text-white text-xl font-bold animate__bounceIn"> @jungie_boi </h1>
     </div>
   );
 }
--- src/components/Social-Icons.js
+++ src/components/Social-Icons.js
@@ -5,7 +5,7 @@ import { faCoffee, faPaintBrush } from "@fortawesome/free-solid-svg-icons";
 function SocialIcons() {
   return (
     <div className="social-icons flex justify-evenly gap-4 my-10">
-      <FontAwesomeIcon icon={faTwitch} size="3x" />
+      <FontAwesomeIcon className="" icon={faTwitch} size="3x" />
       <FontAwesomeIcon icon={faTwitter} size="3x" />
       <FontAwesomeIcon icon={faPaintBrush } size="3x" />
       <FontAwesomeIcon icon={faCoffee} size="3x" />
