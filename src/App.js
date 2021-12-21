2cfa0dabcd99bc185a83a2e232cc975eec198509
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2021-12-21 09:35:23 -0600

    removed unused code in App.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,9 @@ import FFXIVbox from "./components/FFXIVbox";
 
 
 function App() {
-  // initMobileQuery(width, el);
-  //width.addEventListener(initMobileQuery);
   let width = window.matchMedia("(min-width: 768px)");
   const loadAnim = (width.matches) ? 'animate__animated animate__zoomIn':'';
+
   return (
     <div className="app flex flex-col justify-center items-center">
       <div className="layout-wrapper md:flex md:w-full md:justify-evenly md:items-center">
