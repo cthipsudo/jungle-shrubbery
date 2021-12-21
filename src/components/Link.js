4d401b54d4265050146c7ab01e28e644fee0949a
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2021-12-21 10:41:39 -0600

    Removed unused index and prepped for scroll animation

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useRef, useLayoutEffect} from "react";
 
-function Link({ link, text, target, mobileOnly, index }) {
-  let c_class = (mobileOnly) ? 'link-mobile' : ''
+function Link({ link, text, target, mobileOnly }) {
+  let c_class = (mobileOnly) ? 'link-mobile' : '';
+  const refRef = useRef(null);
+  
   return (
-    <li className={`text-white animate__animated animate__zoomIn ${c_class}`}>
+    <li ref={refRef} className={`text-white animate__animated animate__zoomIn ${c_class}`}>
       <a target={target} href={link}>
         <button className="font-bold font-sans">{text}</button>
       </a>
